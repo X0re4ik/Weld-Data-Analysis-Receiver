59ab2b93e679d79f56a2526836ec6debff251d73
Xoooore4ik <xoore4ik@gmail.com>
2023-07-28 13:48:37 +0500

    Выполнена рабочая версия приемника данных на 2-ух разных протоколах: MQTT и HTTP/HTTPS. Написан эмулятор данных. Осталась только документация.

--- RWSensorSimulator/RWSensorSimulator.py
+++ RWSensorSimulator/RWSensorSimulator.py
@@ -50,25 +50,32 @@ class RWSensorSimulator:
         
         self.__list_to_send_to_server.append(data)
     
-    def _send_to_server(self, ip="localhost", http_port=None, mqtt_port=None):
+    def _send_to_server(self, ip="localhost", http_port=None, mqtt_port=None, username=None, password=None):
         data = json.dumps(self.__list_to_send_to_server)
         if http_port:
             http_url = f"http://{ip}:{http_port}/write_values"
             r = requests.post(http_url, json=data)
         
-        else:
+        elif mqtt_port:
             from  receiver.mqtt_manager.publisher import MQTTPublisher
             
             def __callback(client):
                 topic = f"RW/{self.__mac_address}"
                 result = client.publish(topic, data)
             
-            MQTTPublisher(broker=ip, callback=__callback).run()
+            MQTTPublisher(
+                broker=ip, port=mqtt_port, 
+                username=username, password=password, 
+                callback=__callback).run()
 
-    def start_simulation(self):
+    def start_simulation(self, ip="localhost", http_port=None, mqtt_port=None, username=None, password=None):
+        #print(ip, http_port, mqtt_port, username, password)
         while True:
             if len(self.__list_to_send_to_server) > self.__class__.MAXIMUM_NUMBER_OF_RECORDS:
-                self._send_to_server()
+                self._send_to_server(
+                    ip, http_port=http_port, mqtt_port=mqtt_port, 
+                    username=username, password=password)
+                
                 self.__list_to_send_to_server = []
             self._read_values_from_source()
             time.sleep(1)
--- run.py
+++ run.py
@@ -1,38 +1,50 @@
-from collections.abc import Callable, Iterable, Mapping
-from typing import Any
-from receiver import app
+import json
+import os
 
-        
+from dotenv import load_dotenv    
 from multiprocessing import Process
 
+from receiver import app
 from receiver.mqtt_manager.subscriber import MQTTSubscription
+from receiver.rw_device_manager import rw_device_manager
+
 
 class HTTPServerFastData(Process):
-    
     def run(self) -> None:
-        from receiver import app
-        app.run(debug=True)
-        pass
+        app.run(
+            host=os.environ["HOST"],
+            port=int(os.environ["HTTP_PORT"]),
+            debug=(os.environ["DEBUG"] == '1'))
     
-from receiver.rw_device_manager import rw_device_manager
-import json
+
 class MQTTServerFastData(Process):
-    
     def run(self) -> None:
         
         def __callback(client, userdata, msg):
             for i in json.loads(msg.payload.decode()):
-                print(i)
                 rw_device_manager.add(i)
         
-        MQTTSubscription(broker="localhost", callback=__callback).subscribe("RW/#").run()
+        broker      = os.environ["HOST"]
+        port        = int(os.environ["MQTT_PORT"])
+        username    = os.environ["MQTT_USERNAME"]
+        password    = os.environ["MQTT_PASSWORD"]
+        
+        
+        MQTTSubscription(
+            broker=broker, port=port, 
+            username=username, password=password, 
+            callback=__callback).subscribe(os.environ["MQTT_TOPIC"]).run()
 
  
     
 def _main():
-    #HTTPServerFastData().start()
-    MQTTServerFastData().start()
-    
+    if os.environ["USING_PROTOCOL"] == 'http':
+        HTTPServerFastData().start()
+    else:
+        MQTTServerFastData().start()
+
+
+load_dotenv(".env")
 
 if __name__ == "__main__":
     _main()
