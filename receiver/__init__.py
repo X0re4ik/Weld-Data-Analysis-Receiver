07ca9bfe3631c71a3e48f64ba2cfbb690115daba
Xoooore4ik <xoore4ik@gmail.com>
2023-08-10 16:19:04 +0500

    Исправлена проблема с многопоточной обработкой.

--- receiver/__init__.py
+++ receiver/__init__.py
@@ -1,41 +1,26 @@
-from flask import Flask, request
+import json
+
+from flask import Flask, request, Response
+from werkzeug.datastructures import Headers
+from datetime import datetime
+
 from receiver.rw_device_manager import rw_device_manager
-
-
-import logging
-
 
 app = Flask(__name__)
 
-import json
-import time
-
 @app.route('/write_values', methods=["POST"])
 def write_values():
-    #request.authorization["username"]
-    data = json.loads(request.get_json())
-    ID = data["ID"]
-    measurements = data["measurements"]
-    now = time.time()
-    for measurement in measurements:
-        measurement.update({"ID": ID})
-        rw_device_manager.add(measurement)
-    end = time.time()
-    return {
-        "success": True,
-        "period": end - now
-    }
-
-from flask import Response
-@app.route('/test_write_values', methods=["POST"])
-def test_write_values():
-    print(request.is_json)
-    print(request.get_json())
     
-    answer = json.dumps({
-        'state': 'ON'
-    })
-    return Response(answer, status=200)
-
-
-
+    data = request.get_json()
+    rw_device_manager.add(data)
+    
+    response = json.dumps(
+        {
+            "data-accepted": True,
+            "current-time": datetime.now().strftime("%d.%m.%Y-%H:%M:%S"),
+            "gas-type": "1"
+        })
+    
+    headers = Headers()
+    headers.add('Content-Type', 'application/json')
+    return Response(response, 200, headers)
